ab4a71360ab26a9b266c7b3e66ecc427f6f0241a
Arsenii <arspopov01@gmail.com>
2025-01-15 09:36:38 +0100

    feat: add script to task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,35 @@
+// 03-files-in-folder/index.js
+
+const fs = require('fs/promises');
+const path = require('path');
+
+async function showFilesInfo() {
+  try {
+    const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+    const dirents = await fs.readdir(secretFolderPath, { withFileTypes: true });
+
+    for (const dirent of dirents) {
+      if (dirent.isFile()) {
+        const fileName = dirent.name;
+
+        const filePath = path.join(secretFolderPath, fileName);
+
+        const stats = await fs.stat(filePath);
+
+        const fileSize = stats.size;
+
+        const ext = path.extname(fileName);
+        const baseName = path.basename(fileName, ext);
+
+        const extWithoutDot = ext.slice(1);
+
+        console.log(`${baseName} - ${extWithoutDot} - ${fileSize} bytes`);
+      }
+    }
+  } catch (err) {
+    console.error('Ошибка при чтении папки или файла:', err);
+  }
+}
+
+showFilesInfo();
